75684a9b6b5cfb40ad0c2752916934f897690fbe
gary091239 <gary091239@gmail.com>
2018-06-16 02:11:13 +0800

    basic redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "url#index"
+  post '/url',to: 'url#create'
+  get '/:url' ,to: 'url#redirect'
 end
